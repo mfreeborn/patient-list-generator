5f063713ed76a0191616da6c4bf267f45cc6438b
Michael Freeborn <michaelfreeborn1@gmail.com>
2020-01-17 13:32:06 +0000

    gui improvements

--- app/gui.py
+++ app/gui.py
@@ -1,7 +1,9 @@
 import datetime
 import os
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
+import queue
 
 import PySimpleGUI as sg
 
@@ -10,6 +12,8 @@ from app.teams import TEAMS, Team
 from app.utils import log_gui
 
 sg.theme("Dark Blue 3")
+LOGS_DIR = Path.cwd() / "logs"
+LOGS_DIR.mkdir(exist_ok=True)
 
 main_tab_layout = [
     [sg.Text("Choose input file:*")],
@@ -44,16 +48,37 @@ main_tab_layout = [
     ],
     [
         sg.Text(
-            "Error - please check the logs",
+            " Error - please check the logs",
             key="error_text",
             visible=False,
             font=("Helvetica", 12, "bold"),
+        )
+    ],
+    [
+        sg.Text(
+            " List generated successfully",
+            key="list_success_text",
+            font=("Helvetica", 11),
+            visible=False,
         )
     ],
 ]
 
 log_tab_layout = [
     [sg.Output(key="output", size=(65, 15))],
+    [
+        sg.Button("Save", key="save_logs_button"),
+        sg.Button("Open Logs", key="open_logs_folder"),
+    ],
+    [
+        sg.Text(
+            "Logs saved successfully",
+            key="logs_success_text",
+            font=("Helvetica", 10),
+            size=(65, 2),
+            visible=False,
+        )
+    ],
 ]
 
 credentials_tab_layout = [
@@ -76,6 +101,14 @@ credentials_tab_layout = [
         ),
     ],
     [sg.Button("Set Credentials", key="set_credentials")],
+    [
+        sg.Text(
+            " Credentials set successfully",
+            key="credentials_success_text",
+            font=("Helvetica", 11),
+            visible=False,
+        ),
+    ],
     [
         sg.Text(
             "Note: credentials are never saved to disk, they are held\nin memory "
@@ -107,107 +140,157 @@ credentials = {
 }
 
 
+def open_folder(path: str):
+    if os.name == "nt":
+        # Windows
+        os.startfile(path)
+    else:
+        # Linux
+        subprocess.Popen(["xdg-open", path])
+
+
+def _generate_list(queue, values):
+    team = values["selected_team"]
+    input_file_path = Path(values["input_file_path"])
+    output_file_path = Path(values["output_folder_path"]) / values["output_filename"]
+    queue.put("start_generating_list")
+    try:
+        main(
+            team=team,
+            credentials=credentials,
+            input_file_path=input_file_path,
+            output_file_path=output_file_path,
+        )
+
+    except Exception as e:
+        print(e)
+        queue.put("error_generating_list")
+    else:
+        queue.put("finish_generating_list")
+
+
 def run_gui():
     main_window = sg.Window("Patient List Generator", layout, finalize=True)
 
     # switch to the Credentials tab on startup
     main_window["tab_group"].Widget.select(1)
+    main_window["careflow_username"].set_focus()
+    # bind return key to credential input boxes
+    for cred in credentials:
+        main_window[cred].bind("<Return>", "set_credentials")
 
-    while True:
-        event, values = main_window.read()
+    gui_queue = queue.Queue()
+    with ThreadPoolExecutor(max_workers=1) as executor:
+        while True:
+            event, values = main_window.read(timeout=100)
 
-        if event is None:
-            break
+            if event is None:
+                break
+            elif event != "__TIMEOUT__":
+                log_gui(event, values)
 
-        log_gui(event, values)
-        main_window["error_text"].update(visible=False)
+                main_window["error_text"].update(visible=False)
+                main_window["list_success_text"].update(visible=False)
+                main_window["credentials_success_text"].update(visible=False)
+                main_window["logs_success_text"].update(visible=False)
 
-        if event == "input_file_path":
-            if values["input_file_path"]:
-                path = Path(values["input_file_path"])
-                parent_path = path.parent
-                file_ext = path.suffix
+                if event == "input_file_path":
+                    if values["input_file_path"]:
+                        path = Path(values["input_file_path"])
+                        parent_path = path.parent
+                        file_ext = path.suffix
 
-                today = datetime.date.today()
-                if not values["output_folder_path"]:
-                    main_window["output_folder_path"].update(parent_path)
-                    main_window["open_output_folder"].update(disabled=False)
+                        if not values["output_folder_path"]:
+                            main_window["output_folder_path"].update(parent_path)
+                            main_window["open_output_folder"].update(disabled=False)
 
-        if event == "open_output_folder":
-            if not values["output_folder_path"]:
-                continue
+                if event == "open_output_folder":
+                    if not values["output_folder_path"]:
+                        continue
 
-            path = values["output_folder_path"]
+                    path = values["output_folder_path"]
+                    open_folder(path)
 
-            if os.name == "nt":
-                # Windows
-                os.startfile(path)
-            else:
-                # Linux
-                subprocess.Popen(["xdg-open", path])
+                if event == "open_logs_folder":
+                    open_folder(LOGS_DIR)
 
-        if event == "generate_list_button":
-            if values["selected_team"] and values["input_file_path"]:
-                team = values["selected_team"]
-                input_file_path = Path(values["input_file_path"])
-                output_file_path = (
-                    Path(values["output_folder_path"]) / values["output_filename"]
-                )
+                if event == "generate_list_button":
+                    if values["selected_team"] and values["input_file_path"]:
+                        executor.submit(_generate_list, gui_queue, values)
 
-                main_window["generate_list_button"].update(
-                    "GENERATING LIST...", disabled=True
-                )
-                main_window.refresh()
+                if event == "save_logs_button":
+                    filename = f"logs_{datetime.datetime.now():%Y-%m-%d_%H:%M:%S}.txt"
+                    file_path = LOGS_DIR / filename
+                    logs = main_window["output"].Get()
 
-                try:
-                    main(
-                        team=team,
-                        credentials=credentials,
-                        input_file_path=input_file_path,
-                        output_file_path=output_file_path,
+                    with open(file_path, "w") as fh:
+                        fh.writelines(logs)
+
+                    main_window["logs_success_text"].update(
+                        f"Logs saved to:\n{file_path}", visible=True
                     )
 
-                except Exception as e:
-                    print(e)
+                if event.endswith("set_credentials"):
+                    for cred in [
+                        "careflow_username",
+                        "careflow_password",
+                        "trakcare_username",
+                        "trakcare_password",
+                    ]:
+                        credentials[cred] = values[cred].strip()
+                    main_window["credentials_success_text"].update(visible=True)
+
+                if all(
+                    values[cred]
+                    for cred in [
+                        "careflow_username",
+                        "careflow_password",
+                        "trakcare_username",
+                        "trakcare_password",
+                    ]
+                ):
+                    main_window["set_credentials"].update("Update Credentials")
+                else:
+                    main_window["set_credentials"].update("Set Credentials")
+
+                if values["selected_team"] and not isinstance(
+                    values["selected_team"], Team
+                ):
+                    main_window["selected_team"].update(None)
+
+                if values["output_folder_path"]:
+                    main_window["open_output_folder"].update(disabled=False)
+
+                if values["input_file_path"] and isinstance(
+                    values["selected_team"], Team
+                ):
+                    path = Path(values["input_file_path"])
+                    file_ext = path.suffix
+                    main_window["output_filename"].update(
+                        f"{datetime.datetime.today():%d-%m-%Y}_"
+                        f"{values['selected_team'].name.value.lower()}{file_ext}"
+                    )
+
+                    main_window["generate_list_button"].update(disabled=False)
+
+            try:
+                message = gui_queue.get_nowait()
+            except queue.Empty:
+                pass
+            else:
+                if message == "start_generating_list":
+                    main_window["generate_list_button"].update(
+                        "GENERATING LIST...", disabled=True
+                    )
+
+                if message == "finish_generating_list":
+                    main_window["generate_list_button"].update(
+                        "Generate List", disabled=False
+                    )
+                    main_window["list_success_text"].update(visible=True)
+
+                if message == "error_generating_list":
+                    main_window["generate_list_button"].update(
+                        "Generate List", disabled=False
+                    )
                     main_window["error_text"].update(visible=True)
-
-                main_window["generate_list_button"].update(
-                    "Generate List", disabled=False
-                )
-
-        if event == "set_credentials":
-            for cred in [
-                "careflow_username",
-                "careflow_password",
-                "trakcare_username",
-                "trakcare_password",
-            ]:
-                credentials[cred] = values[cred].strip()
-
-        if all(
-            values[cred]
-            for cred in [
-                "careflow_username",
-                "careflow_password",
-                "trakcare_username",
-                "trakcare_password",
-            ]
-        ):
-            main_window["set_credentials"].update("Update Credentials")
-        else:
-            main_window["set_credentials"].update("Set Credentials")
-
-        if values["selected_team"] and not isinstance(values["selected_team"], Team):
-            main_window["selected_team"].update(None)
-
-        if values["output_folder_path"]:
-            main_window["open_output_folder"].update(disabled=False)
-
-        if values["input_file_path"] and isinstance(values["selected_team"], Team):
-            path = Path(values["input_file_path"])
-            file_ext = path.suffix
-            main_window["output_filename"].update(
-                f"{datetime.datetime.today():%d-%m-%Y}_{values['selected_team'].name.value.lower()}{file_ext}"
-            )
-
-            main_window["generate_list_button"].update(disabled=False)
