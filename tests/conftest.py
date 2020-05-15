c0a4b178330b4f0c2a1b0a6bf1d92aaddbce4c96
Michael Freeborn <31806808+mfreeborn@users.noreply.github.com>
2020-05-15 09:49:00 +0100

    add DSU to trakcare bed formats

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,49 +4,6 @@ from app.models import Patient, PatientList
 
 
 @pytest.fixture
-def careflow_bed_format():
-    return [
-        # Alex
-        (Ward.ALEXANDRA, "ALEBAY03", "Bed A", "3A"),
-        (Ward.ALEXANDRA, "ALESR06", "Bed 1", "SR6"),
-        # Capener
-        (Ward.CAPENER, "CAPBAY03", "BedA", "3A"),
-        (Ward.CAPENER, "CAPSR06", "Bed01", "SR6"),
-        # Fortescue
-        (Ward.FORTESCUE, "FOR2YELLOW", "Bed01", "Yellow 1"),
-        (Ward.FORTESCUE, "FOR2GREENBAY", "Bed02", "Green 2"),
-        (Ward.FORTESCUE, "FOR3LILACBAY", "Bed03", "Lilac 3"),
-        (Ward.FORTESCUE, "FOR4BLUEBAY", "Bed04", "Blue 4"),
-        (Ward.FORTESCUE, "FOR5PINKBAY", "Bed05", "Pink 5"),
-        (Ward.FORTESCUE, "FORLILACRM", "Bed01", "SR Lilac"),
-        (Ward.FORTESCUE, "FORYELLOWRM", "Bed01", "SR Yellow"),
-        (Ward.FORTESCUE, "FORBLUERM", "Bed01", "SR Blue"),
-        (Ward.FORTESCUE, "FORPINKRM", "Bed01", "SR Pink"),
-        # Glossop
-        (Ward.GLOSSOP, "GLOBAY03", "BedF", "3F"),
-        (Ward.GLOSSOP, "GLOSR13", "Bed01", "SR13"),
-        # KGV
-        (Ward.KGV, "KGVBAY05", "BedB", "5B"),
-        (Ward.KGV, "KGVSR07", "Bed01", "SR7"),
-        # Lundy
-        (Ward.LUNDY, "CAPBAY01", "Bed1C", "1C"),
-        (Ward.LUNDY, "LUNBAY02", "BedB", "2B"),
-        (Ward.LUNDY, "LUNSR02", "Bed01", "SR2"),
-        # Roborough
-        (Ward.ROBOROUGH, "ROBSR02", "Bed02", "SR2"),
-        # Staples
-        (Ward.STAPLES, "STABAY01", "Bed A", "1A"),
-        (Ward.STAPLES, "STASR02", "Bed 2", "SR2"),
-        # Tarka
-        (Ward.TARKA, "TARBAY02", "Bed2D", "2D"),
-        (Ward.TARKA, "TARSR08", "Bed01", "SR8"),
-        # Victoria
-        (Ward.VICTORIA, "VICBAY02", "BedA", "2A"),
-        (Ward.VICTORIA, "VICSR08", "Bed01", "SR8"),
-    ]
-
-
-@pytest.fixture
 def trakcare_bed_format():
     return [
         # Alex
@@ -61,6 +18,9 @@ def trakcare_bed_format():
         (Ward.CAPENER, "Bay 03 CA", "BedD", "3D"),
         (Ward.CAPENER, "Room 07 CA", "Bed01", "SR7"),
         (Ward.CAPENER, "Lundy Bay on Capener Ward", "Bed1F", "LBOC F"),
+        # Day Surgery Unit
+        (Ward.DAY_SURGERY_UNIT, "DSU Bay 03", "BEDF", "3F"),
+        (Ward.DAY_SURGERY_UNIT, "DSU Room 02", "BED01", "SR2"),
         # Fortescue
         (Ward.FORTESCUE, "Yellow (FORT)", "Bed01", "Yell 1"),
         (Ward.FORTESCUE, "Green (FORT)", "Bed02", "Green 2"),
